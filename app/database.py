af9e45b7e967993052ad1e0b3e410fe1ce39f391
DSM6 <fatec-dsm6@academico.intra>
2025-10-24 22:17:46 -0300

    atividade

---
+++ app/database.py
@@ -0,0 +1,20 @@
+from .schemas import Student
+
+students_db = [
+    Student(id=1, name="Renato Hioji Okamoto Odake", email="renato@example.com")
+]
+
+
+def get_all_students():
+    return students_db
+
+
+def get_student_by_id(student_id: int):
+    return next((s for s in students_db if s.id == student_id), None)
+
+
+def create_student(student):
+    new_id = max(s.id for s in students_db) + 1 if students_db else 1
+    new_student = Student(id=new_id, **student.dict())
+    students_db.append(new_student)
+    return new_student
---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, HTTPException
+from .database import get_all_students, get_student_by_id, create_student
+from .schemas import Student, StudentCreate
+
+app = FastAPI(title="Student API")
+
+@app.get("/students", response_model=list[Student])
+def list_students():
+    return get_all_students()
+
+
+@app.get("/students/{student_id}", response_model=Student)
+def get_student(student_id: int):
+    student = get_student_by_id(student_id)
+    if not student:
+        raise HTTPException(status_code=404, detail="Estudante nao encontrado")
+    return student
+
+
+@app.post("/students", response_model=Student, status_code=201)
+def add_student(student: StudentCreate):
+    return create_student(student)
